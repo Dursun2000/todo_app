d0510e7426d477573d35afa9a13d29cca7590b0f
Dursun <dursunhasanov06@gmail.com>
2026-06-29 23:00:22 +0500

    Добавил генерацию id при создании тестов, при помощи uuid, добавил post запрос который позволяет добавлять новую задачу

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import uvicorn
 from fastapi import FastAPI, status
 from pydantic import BaseModel
+from uuid import uuid4
 
 app = FastAPI()
 
@@ -13,11 +14,18 @@ tasks: list[TaskSchema] = []
 
 class CreateTaskSchema(BaseModel):
     title: str
-    is_completed: bool
+    is_completed: bool = False
 
 @app.get("/tasks", status_code=status.HTTP_200_OK)
 def get_all_tasks():
     return tasks
+
+@app.post("/tasks")
+def create_task(task: CreateTaskSchema):
+    new_task = TaskSchema(id=str(uuid4()), title=task.title, is_completed=task.is_completed)
+    tasks.append(new_task)
+
+    return new_task
 
 if __name__ == '__main__':
     uvicorn.run("main:app", reload=True)
